4365765035c8086cb3c2009c03737a0da0e0b2dc
Pinnoche <ptime552@gmail.com>
2024-06-20 03:24:00 +0100

    baseUrl

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 axios.defaults.withCredentials= true;
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = 'https://pizza-order-server.vercel.app/';
 
 
 
